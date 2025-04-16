a2ff5c2677c1bf36a3d14d03c8a1c9059790a820
Shamayel Torabi <shamayel.torabi@gmail.com>
2025-04-16 18:25:23 +0330

    update room page

--- app/routes/_main.room.$roomId.tsx
+++ app/routes/_main.room.$roomId.tsx
@@ -43,40 +43,39 @@ export default function Room({ loaderData }: Route.ComponentProps) {
     <div className="flex">
       <Card className="flex-1 my-2 ms-2">
         <CardContent>
-          <h1 className="text-gray-900 dark:text-gray-50">Test</h1>
-          <div className="row my-3">
-            <div id="remote-media" className="col mt-3 d-flex justify-content-between mb-3 gap-1" >
-              <div className="remote-video-container border border-primary remote-speaker">
-                <video id="remote-video-1" className="w-100 h-100 remote-video" autoPlay controls></video>
-                <div id="username-1" className="username"></div>
+          <div>
+            <div id="remote-media" className="grid grid-cols-4" >
+              <div className="remote-video-container remote-speaker">
+                <video id="remote-video-1" className="w-32 h-full" autoPlay controls></video>
+                <div id="username-1"></div>
               </div>
-              <div className="remote-video-container border border-primary" style={style}>
-                <video id="remote-video-2" className="w-100 h-100 remote-video" autoPlay controls></video>
-                <div id="username-2" className="username"></div>
+              <div className="remote-video-container" style={style}>
+                <video id="remote-video-2" className="w-32 h-full remote-video" autoPlay controls></video>
+                <div id="username-2"></div>
               </div>
-              <div className="remote-video-container border border-primary" style={style}>
-                <video id="remote-video-3" className="w-100 h-100 remote-video" autoPlay controls></video>
+              <div className="remote-video-container" style={style}>
+                <video id="remote-video-3" className="w-32 h-full remote-video" autoPlay controls></video>
                 <div id="username-3" className="username"></div>
               </div>
-              <div className="remote-video-container border border-primary" style={style}>
-                <video id="remote-video-4" className="w-100 h-100 remote-video" autoPlay controls></video>
+              <div className="remote-video-container" style={style}>
+                <video id="remote-video-4" className="w-32 h-full remote-video" autoPlay controls></video>
                 <div id="username-4" className="username"></div>
               </div>
             </div>
-          </div>
-          <div className="row">
-            <div className="col-10">
-              <div id="current-speaker" className="mb-3" style={{ textAlign: 'center' }}>
-                <div className="current-video-container" style={{ width: "80%", height: "400px", margin: "0 auto" }}>
-                  <video id="remote-video-0" className="w-100 h-100 border border-primary remote-video" autoPlay controls></video>
-                  <div id="username-0" className="username"></div>
+            <div className="my-3 grid grid-cols-[8fr_2fr] gap-1">
+              <div className="w-full">
+                <div id="current-speaker" className="mb-3">
+                  <div className="current-video-container">
+                    <video id="remote-video-0" className="w-full h-full" autoPlay controls></video>
+                    <div id="username-0" className="username"></div>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="col-2">
-              <div id="local-media" className="position-relative" style={{ height: "150px" }}>
-                <div className="position-absolute">
-                  <video id="local-video-left" className="border border-primary" muted autoPlay></video>
+              <div >
+                <div id="local-media" className="position-relative" style={{ height: "150px" }}>
+                  <div className="position-absolute">
+                    <video id="local-video-left" className="border border-primary" muted autoPlay></video>
+                  </div>
                 </div>
               </div>
             </div>
